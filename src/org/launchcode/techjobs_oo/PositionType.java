80be50c0c731c4d8f5f37459924e4411de8e23ba
FlaviaOwens <75456798+FlaviaOwens@users.noreply.github.com>
2021-03-16 17:41:50 -0500

    completed positiontype

--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -17,8 +17,27 @@ public class PositionType {
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
 
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PositionType)) return false;
+        PositionType position = (PositionType) o;
+        return getId() == position.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 
     // Getters and Setters:
 
