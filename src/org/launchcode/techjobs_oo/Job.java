119ff075738c535e5ff73c7a1650a5fd087c504d
FlaviaOwens <75456798+FlaviaOwens@users.noreply.github.com>
2021-03-16 21:39:15 -0500

    Added JobTest class

--- src/org/launchcode/techjobs_oo/Job.java
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -1,4 +1,5 @@
 package org.launchcode.techjobs_oo;
+import java.util.Objects;
 
 public class Job {
 
@@ -15,9 +16,83 @@ public class Job {
     //  other five fields. The second constructor should also call the first in order to initialize
     //  the 'id' field.
 
+    public Job() {
+        id = nextId;
+        nextId++;
+    }
+
+    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
+        this();
+        this.name = name;
+        this.employer = employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
+    }
+
+    // Custom equals and hashCode methods:
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Job)) return false;
+        Job job = (Job) o;
+        return getId() == job.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    // Getters and Setters:
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(Employer value) {
+        this.employer = value;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location value) {
+        this.location = value;
+    }
+
+    public PositionType getPositionType() {
+        return positionType;
+    }
+
+    public void setPositionType(PositionType value) {
+        this.positionType = value;
+    }
+
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
     //  match.
 
+    public CoreCompetency getCoreCompetancy() {
+        return coreCompetency;
+    }
+
     // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
     //  and id.
+    public void setCoreCompetancy(CoreCompetency value) {
+        this.coreCompetency = value;
+    }
 }
